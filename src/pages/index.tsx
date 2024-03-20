119a66a64c1c3ca5c2d1c85da980c2c94762b249
Ruben Dario Duarte Anaya <ruben.duarte.anaya@gmail.com>
2024-03-20 11:05:12 -0500

    change home view

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,44 +1,28 @@
-import Problemtop from "@/components/Problemtop/Problemtop";
-import Head from "next/head";
-import ProblemsList from "@/components/ProblemsList/ProblemsList";
+import { AuthModalState } from '@/atoms/authModalAtom';
+import Navbar from '@/components/Navbar/Navbar';
+import AuthModal from '@/components/modals/AuthModal';
+import React from 'react';
+import { useRecoilValue } from 'recoil';
+import Image from 'next/image';
 
-export default function Home() {
-  return (
-      <>
+type AuthPageProps = { };
 
-        <main className="bg-background-blue min-h-screen">
-              <Problemtop/>
-              <h1
-                className='text-2xl text-center text-gray-700 dark:text-gray-400 font-medium  mt-10 mb-5'>
-                  Iniciemos la aventura 🎼 etude-1: Bambuco
-              </h1>
-
-              <div className='relative overflow-x-auto mx-auto px-6 pb-10'>
-              <table className='text-sm text-left text-gray-500 dark:text-gray-400 sm:w-7/12 w-full max-w-[1200px] mx-auto'>
-                <thead className='text-xs text-gray-700 uppercase dark:text-gray-400 border-b '>
-                  <tr>
-                    <th scope='col' className='px-1 py-3 w-0 font-medium'>
-                      Estado
-                    </th>
-                    <th scope='col' className='px-6 py-3 w-0 font-medium'>
-                      Titulo
-                    </th>
-                    <th scope='col' className='px-6 py-3 w-0 font-medium'>
-                      Dificultad
-                    </th>
-
-                    <th scope='col' className='px-6 py-3 w-0 font-medium'>
-                      Categoria
-                    </th>
-                    <th scope='col' className='px-6 py-3 w-0 font-medium'>
-                      Pista
-                    </th>
-                  </tr>
-                </thead>
-                <ProblemsList/>
-              </table>
-              </div>
-        </main>
-      </>    
-  );
+const AuthPage:React.FC<AuthPageProps> = () => {
+    const authModal = useRecoilValue(AuthModalState)
+    return <div className='bg-background-blue h-screen relative'>
+                        <div className='max-w-7xl mx-auto'>
+                            <Navbar/>
+                            <div className="slogan bg-light-blue py-0  rounded text-white h-80 opacity-20"> 
+                            </div>   
+                                <div className="text absolute py-8 px-4  rounded bottom-40 right-20 w-72 z-10  ">
+                                    <h1 className='flex text-4xl font-light justify-center my-10 ' > </h1>
+                                        <p className='flex text-3xl  text-center text-white font-bold  relative z-10 mb-4 '>Estudia, practica y resuelve retos de código </p> 
+                                        <p className=' flex text-1xl text-center text-dark-yellow relative z-10  '>con roadmap optimizado, puzzles y space repetition! de Co para el 🌎 </p>     
+                                </div>       
+                                <Image src="/puzzles.jpg" alt="coding Books" height={500} width={950} className="bg-cover h-screen absolute bottom-0 right-0 opacity-40"/> 
+                                <Image src="/coding.jpg" alt="coding" className='w-96 py-0 rounded  absolute bottom-0 z-10 opacity-30 mx-0 ' height={500} width={950}/>
+                                {authModal.isopen && <AuthModal/>}
+                        </div>
+                </div>;
 }
+export default AuthPage;
